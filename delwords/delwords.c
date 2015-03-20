eea34b8670e4511d5b16c5f75e63cd2048ab0957
Moklev Slava <slavam2605@bk.ru>
2015-03-20 11:48:00 -0400

    second try

--- delwords/delwords.c
+++ delwords/delwords.c
@@ -21,27 +21,56 @@ int prefix(char* s, char* p)
 int main(int argc, char** argv) 
 {
     char* pattern = argv[1];
-    char* a = (char*) malloc(MAX_BUF_SIZE);
-    int res, res2;
+    char* s  = (char*) malloc(MAX_BUF_SIZE);
+    int res, res2, res3;
     int len;
     int p_len = strlen(pattern);
+    char* a = s;
+    char* b = pattern;
+    char* cc = (char*) malloc(strlen(pattern));
+    strcpy(cc, pattern);
+    printf("lol\n");
+    int rest_c = 0;
+    int init_len = strlen(pattern);
     while ((res = read_(STDIN_FILENO, a, MAX_BUF_SIZE)) > 0) {
         while (1) {
             len = 0;    
-            while ((res2 = prefix(a + len, pattern)) == 0)
+            while ((res2 = prefix(a + len, pattern)) == 0) {
                 len++;
-            if (write_(STDOUT_FILENO, a, len) < 0) {
+                res--;
+                write_(STDOUT_FILENO, cc, rest_c);
+                rest_c = 0;
+                pattern = b;
+                p_len = init_len;
+            }
+            if (res3 = write_(STDOUT_FILENO, a, len) < 0) {
                 perror("error: ");
-                return 0;
+                return res3;
             }
             a = a + len;
+            if (res2 == 2) { 
+                a = a + p_len;
+                rest_c = 0;
+                pattern = b;
+                p_len = init_len;
+            }
+            if (a == s + MAX_BUF_SIZE || res2 == 1) {
+                int c_len = MAX_BUF_SIZE - (a - s);
+                a = s;
+                int i;
+                for (i = 0; i < c_len; i++) {
+                    cc[i] = pattern[i];
+                }
+                rest_c = c_len;
+                pattern += c_len;
+                p_len = strlen(pattern);
+                break;
+            }
             if (res2 != 2)
                 break;
-            else
-                a = a + p_len;
         }
     }
     if (res < 0) 
         perror("error: ");
-    return 0;
+    return res;
 }
