eb23b36e5871781ec2281be7978ebfa6aa012045
Moklev Slava <slavam2605@bk.ru>
2015-03-24 22:02:46 +0300

    last line fixed

--- filter/filter.c
+++ filter/filter.c
@@ -7,14 +7,17 @@
 int main(int argc, char** argv)
 {
     char a[MAX_LINE_SIZE];
-    char s[MAX_LINE_SIZE];
+    char s[MAX_LINE_SIZE + 1];
     char* args[argc + 1];
     int i;
     int res;
     int rc;
     while ((res = read_until(STDIN_FILENO, a, MAX_LINE_SIZE, '\n')) > 0) {
         strncpy(s, a, res);
-        s[res - 1] = '\0';
+        if (s[res - 1] == '\n')
+            s[res - 1] = '\0';
+        else
+            s[res] = '\0';
         for (i = 1; i < argc; i++)
             args[i - 1] = argv[i];
         args[argc - 1] = s;
