2266bb4542ba9f715f4faa9cc52a3c7b985c9fc1
Moklev Slava <slavam2605@bk.ru>
2015-03-31 14:00:40 +0300

    fixed something

--- bufcat/bufcat.c
+++ bufcat/bufcat.c
@@ -5,8 +5,11 @@
 int main(int argc, char** argv)
 {
     struct buf_t * buf = buf_new(BUF_SIZE);
-    ssize_t res;
-    while ((res = buf_fill(STDIN_FILENO, buf, BUF_SIZE + 1)) > 0)
-        buf_flush(STDOUT_FILENO, buf, BUF_SIZE);
-    buf_flush(STDOUT_FILENO, buf, BUF_SIZE);
+    ssize_t res, res2, res3;
+    while ((res = buf_fill(STDIN_FILENO, buf, BUF_SIZE)) > 0)
+        res2 = buf_flush(STDOUT_FILENO, buf, BUF_SIZE);
+    res3 = buf_flush(STDOUT_FILENO, buf, BUF_SIZE);
+    if (res < 0 || res2 < 0 || res3 < 0)
+        return -1;
+    return 0;
 }
